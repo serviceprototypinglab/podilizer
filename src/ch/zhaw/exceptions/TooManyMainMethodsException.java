1a151fd8984592e1992bab6cd99a804ed69e2722
dord <dord@zhaw.ch>
2016-09-28 13:28:17 +0200

    added MethodEntity; added method calls finding

--- src/ch/zhaw/exceptions/TooManyMainMethodsException.java
+++ src/ch/zhaw/exceptions/TooManyMainMethodsException.java
@@ -1,8 +1,8 @@
-package ch.zhaw.file_operations;
+package ch.zhaw.exceptions;
 
 /**
  * Created by dord on 9/28/16.
  */
 public class TooManyMainMethodsException extends Exception {
-
+    // TODO: 9/28/16 feel the exception
 }
